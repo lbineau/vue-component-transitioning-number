da5a9cafee8dba59ce19467bbcde212781d87e45
lbineau <lory.bineau@gmail.com>
2023-11-08 22:32:39 +0100

    upgrage from vue-cli to vite, vue 2 to 3

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,13 @@
-<template>
-  <div id="app">
-    <input type="number" name="" id="" v-model="value" min="0" max="9">
-    Price: <TransitioningNumber :end-value="value" />€
-  </div>
-</template>
-
-<script>
+<script setup>
+import { ref } from 'vue'
 import TransitioningNumber from './components/TransitioningNumber.vue'
-
-export default {
-  name: 'app',
-  data () {
-    return {
-      value: 8
-    }
-  },
-  components: {
-    TransitioningNumber
-  }
-}
+const value = ref(8)
 </script>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-  font-size: 6em;
-  input {
-    display: block;
-    font-size: 1em;
-    margin: 0 auto;
-  }
-}
-</style>
+<template>
+  <div>
+    <label for="number">Label: </label>
+    <input type="number" name="number" id="number" v-model="value" min="0" max="9">
+  </div>
+  Price: <TransitioningNumber :end-value="value" />€
+</template>
